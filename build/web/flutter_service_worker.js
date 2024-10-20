77a90c7d258c32204c3623bc6fd631cbcbda2350
mcaoptimizers <mcaoptimizers@gmail.com>
2024-10-20 19:49:29 +0200

    Ensure all files are updated for GitHub Pages deployment

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5339852eee7b71dc1ce6398849654f0e",
+const RESOURCES = {"flutter_bootstrap.js": "80c02f0f209626a530f77835e9316f78",
 "version.json": "248072a1db9f4259da23d796cd607671",
 "index.html": "12f2d0896140f6d8cd6ddb1f900a567a",
 "/": "12f2d0896140f6d8cd6ddb1f900a567a",
